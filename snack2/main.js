d7c454d409f522b27ad51c6ccf5a6e2a78fdea88
Dario0122 <dario.di.cuia@gmail.com>
2021-11-08 14:32:23 +0100

    snack2

---
+++ snack2/main.js
@@ -0,0 +1,11 @@
+// A partire da un array di numeri, genera un secondo array con le potenze al quadrato di ogni numero.
+// Es: [1, 2, 3, 4, 5] => [1, 4, 9, 16, 25]
+
+const numbers = [5, 7, 15, 43, 65]
+console.log('numeri', numbers);
+
+
+const squaredNumbers = numbers.map( (number) => {
+    return Math.pow(number, 2)
+})
+console.log('Numeri elevati al quadrato', squaredNumbers);
